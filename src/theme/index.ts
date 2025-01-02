c3a8ac9cabb516d643711f12ef95c2f5bf8605f9
Elliot Braem <elliot@ejlbraem.com>
2025-01-02 17:11:18 -0600

    remove redundant theme file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,24 @@
-export * from "./theme";
+import { extendTheme } from "@chakra-ui/react";
+
+import { colors } from "./colors";
+import { config } from "./config";
+import { styles } from "./styles";
+import { fonts } from "./fonts";
+import { breakpoints } from "./breakpoints";
+
+import { ButtonStyle } from "./components/button";
+import { ProgressStyle } from "./components/progress";
+import { SwitchStyle } from "./components/switch";
+
+export const theme = extendTheme({
+  colors,
+  config,
+  styles,
+  fonts,
+  breakpoints,
+  components: {
+    Button: ButtonStyle,
+    Progress: ProgressStyle,
+    Switch: SwitchStyle,
+  },
+});
